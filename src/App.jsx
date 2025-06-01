4fb3b7faaa5aab58b8ddc005aec4b8d75bb18c4e
Tanja Valkin <tanjavalkin@gmail.com>
2025-06-01 11:02:31 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Amplify } from 'aws-amplify';
+import outputs from '../amplify_outputs.json';
+import { MapView } from '@aws-amplify/ui-react-geo';
+
+import '@aws-amplify/ui-react-geo/styles.css';
+
+//import awsExports from './aws-exports.json';
+
+
+
+function App() {
+  Amplify.configure(outputs);
+ // Amplify.configure(awsExports);
+  
+
+  return <MapView/>
+}
+
+export default App
